1a68c584f4e61d3de8fe125c0b1f5e6e2fd1b62a
Isaac Rendon <isaacrendon7@gmail.com>
2020-08-14 20:37:42 -0400

    Blackjack1.0

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Blackjack
+namespace BlackJackCSp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Player
     {
-      Console.WriteLine("Welcome to C#");
+
     }
-  }
+    class Dealer
+    {
+
+    }
+
+    class Card
+    {
+
+    }
+
+    class Program
+    {
+        static List<string> ShuffledDeck()
+        {
+            var randomNumberGenerator = new Random();
+            var ranks = new string[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var suits = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
+            var deck = new List<string>();
+            foreach (var rank in ranks)
+            {
+                foreach (var suit in suits)
+                {
+                    deck.Add($"{rank} of {suit}");
+                }
+            }
+            for (var index = deck.Count() - 1; index >= 0; index--)
+            {
+                var randomNumber = randomNumberGenerator.Next(52);
+                var swapCards = deck[index];
+                deck[index] = deck[randomNumber];
+                deck[randomNumber] = swapCards;
+            };
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card);
+            }
+            return deck;
+        }
+
+        static void DisplayGreeting()
+        {
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+        }
+    }
 }
